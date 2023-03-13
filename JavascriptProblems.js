2c5ee5cb9174add9951dd0487924b99db0798f0c
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2023-03-13 18:58:38 +0530

    [Jyoti Rao] Find Prime number

--- JavascriptProblems.js
+++ JavascriptProblems.js
@@ -409,3 +409,23 @@ for (j=1; j<=harmonicNumber; j++)
   }
  console.log("\nSum of all "+ harmonicNumber +" Harmonic Numbers: " + res); 
  console.log("--------------------------------------------"); 
+
+ //3. Write a program that takes a input and determines if the number is a prime
+let primeNum = Math.floor(Math.random()*10)+2;
+let countValue=0;
+for (let m = 1; m <=primeNum; m++)
+{
+   if (primeNum % m == 0)
+   {
+    countValue++;
+   }
+}
+if(countValue == 2)
+{ 
+   console.log(primeNum + " is prime number");
+}
+else 
+{ 
+   console.log(primeNum + " is not prime number");
+}
+console.log("--------------------------------------------"); 
