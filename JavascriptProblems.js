8991c90932fde6e4223967f1abeb4422ffede8fe
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2023-03-13 19:01:20 +0530

    [Jyoti Rao] Find Prime Factor number

--- JavascriptProblems.js
+++ JavascriptProblems.js
@@ -429,3 +429,20 @@ else
    console.log(primeNum + " is not prime number");
 }
 console.log("--------------------------------------------"); 
+
+//4.Write a program to compute Factors of a number N using prime factorization method.
+let primeFactorNum=18;
+console.log("Prime factor are:-\n--------------------");
+for (let k = 2; k <= primeFactorNum; k++)
+{
+    while (primeFactorNum % k == 0)
+    {
+        primeFactorNum = primeFactorNum / k;
+        console.log(k);
+    }
+}
+if (primeFactorNum > 2)
+ {
+    console.log( primeFactorNum);
+ }
+ console.log("--------------------------------------------"); 
