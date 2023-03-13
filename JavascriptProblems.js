c40c3304ab96ed66f9abf0caa0d4b4d159bc9f7e
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2023-03-13 18:26:57 +0530

    [Jyoti Rao] Perform arithmetic operation

--- JavascriptProblems.js
+++ JavascriptProblems.js
@@ -225,3 +225,34 @@ console.log("--------------------------------------------");
  {
     console.log("Please enter number between 1 to 7 because number of week day are 7");
  } 
+ console.log("--------------------------------------------"); 
+
+ //3.Enter 3 Numbers do following arithmetic operation and find the one that is maximum and minimum
+ //1. a + b * c        
+ let a=10;
+ let b=20;
+ let c=15;
+ let output= a + b * c ;
+ console.log("Result1 are : " + output); 
+
+ //2. a % b + c 
+ let a1=10;
+ let b1=20;
+ let c1=15;
+ let output1= ((a1 % b1) + c1 );
+ console.log("Result2 are : " + output1); 
+
+ //3. c + a / b 
+ let a2=10;
+ let b2=20;
+ let c2=15;
+ let output2= (c2 + (a2 / b2));
+ console.log("Result3 are : " + output2); 
+
+ //4. a * b + c
+ let x=10;
+ let y=20;
+ let z=15;
+ let output3= ((x * y) + z);
+ console.log("Result4 are : " + output3); 
+ console.log("--------------------------------------------"); 
