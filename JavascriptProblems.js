8dbef4d609d9ab2d895e4bb85480ac9558c57d1f
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2023-03-13 18:17:43 +0530

    [Jyoti Rao] Read number and write in word

--- JavascriptProblems.js
+++ JavascriptProblems.js
@@ -144,3 +144,48 @@ while (count < numflip)
 console.log("Total Number of Heads: " + head);
 console.log("Total Number of Tails: " + tail); 
 console.log("--------------------------------------------"); 
+
+//Selection Practice Problems with if else if and else
+ //1.Read a single digit number and write the number in word 
+ let num=Math.floor(Math.random() * 10);
+ if(num==0)
+ {
+    console.log("zero");
+ }
+ else if(num==1)
+ {
+    console.log("One");
+ }
+ else if(num==2)
+ {
+    console.log("Two");
+ }
+ else if(num==3)
+ {
+    console.log("Three");
+ }
+ else if(num==4)
+ {
+    console.log("Four");
+ }
+ else if(num==5)
+ {
+    console.log("five");
+ }
+ else if(num==6)
+ {
+    console.log("six");
+ }
+ else if(num==7)
+ {
+    console.log("seven");
+ }
+ else if(num==8)
+ {
+    console.log("eight");
+ }
+ else if(num==9)
+ {
+    console.log("nine");
+ }  
+ console.log("--------------------------------------------"); 
