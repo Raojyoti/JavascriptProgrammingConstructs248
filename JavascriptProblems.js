f862f227811af90b68d6b103c6c9f2e567934efc
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2023-03-13 19:12:29 +0530

    [Jyoti Rao] Find degF or degC based on their Conversion Selection

--- JavascriptProblems.js
+++ JavascriptProblems.js
@@ -468,3 +468,26 @@ while (n<=powervalue)
     n++
    } 
 console.log("--------------------------------------------");    
+
+//Functions Practice Problems
+//1.Help user find degF or degC based on their Conversion Selection. Use Case Statement and ensure that the inputs are within the Freezing Point (  0 °C / 32 °F ) and the Boiling Point of Water ( 100 °C / 212 °F )
+   //a. degF = (degC * 9/5) + 32
+   //b. degC = (degF – 32) * 5/9
+
+   function findDegF(degC)
+   {
+      degF = (degC * 9/5) + 32;
+      return degF;
+   }
+   let x1=findDegF(100)
+   console.log("Result in degF: " + x1+"F");
+
+   function findDegC(degF)
+   {
+      degC=(degF - 32) * 5/9;
+      return degC;
+   }
+   let y1=findDegC(212)
+   console.log("Result in degF: " + y1+"C"); 
+
+console.log("--------------------------------------------");    
