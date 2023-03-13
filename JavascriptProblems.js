5f7bf758851699b4d4a44ad1e92d36fd0658e871
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2023-03-13 18:37:18 +0530

    [Jyoti Rao] Read number and display week of day

--- JavascriptProblems.js
+++ JavascriptProblems.js
@@ -303,3 +303,40 @@ switch(number)
             console.log("Word ==> Nine");
             break;
 } 
+console.log("--------------------------------------------"); 
+
+//2. Read a Number and Display the week day (Sunday, Monday,…)
+
+let dayNum = Math.floor(Math.random()*7)+ 1;
+switch(dayNum)
+{
+    case 1:
+            console.log("Generate random number : "+ dayNum);
+            console.log("Day ==> Sunday");
+            break;
+    case 2:
+            console.log("Generate random number : "+ dayNum);
+            console.log("Day ==> Monday");
+            break;
+    case 3:
+            console.log("Generate random number : "+ dayNum);
+            console.log("Day ==> Tuesday");
+            break;
+    case 4:
+            console.log("Generate random number : "+ dayNum);
+            console.log("Day ==> Wednesday");
+            break;
+    case 5:
+            console.log("Generate random number : "+ dayNum);
+            console.log("Day ==> Thrusday");
+            break;
+    case 6:
+           console.log("Generate random number : "+ dayNum);
+            console.log("Day ==> Friday");
+            break;
+    case 7:
+            console.log("Generate random number : "+ dayNum);
+            console.log("Day ==> Saturday");
+            break;
+} 
+console.log("--------------------------------------------"); 
