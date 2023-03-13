b20d2368cb0b204e189c857334e209e7d098cc3f
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2023-03-13 18:12:10 +0530

    [Jyoti Rao] Print out number of heads and tails

--- JavascriptProblems.js
+++ JavascriptProblems.js
@@ -119,3 +119,28 @@ else
    console.log(year+" is Not a Leap year");
 } 
 console.log("--------------------------------------------"); 
+
+//4. Write a program to simulate a coin flip and print out "Heads" or "Tails" accordingly
+let head = 0; 
+let tail = 0;
+let count = 0;
+let numflip=5;
+while (count < numflip)
+{
+   let result=Math.floor(Math.random() * 2);
+   console.log(result);
+   if (result < 1)
+   {
+       head++;
+       console.log("head");
+   }
+   else
+   {
+       tail++;
+       console.log("tails");
+   }
+   count++;
+}
+console.log("Total Number of Heads: " + head);
+console.log("Total Number of Tails: " + tail); 
+console.log("--------------------------------------------"); 
