9572a9bd6a7c18a0509eecc91d89bfc703364d17
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2023-03-13 17:50:02 +0530

    [Jyoti Rao] Unit conversion

--- JavascriptProblems.js
+++ JavascriptProblems.js
@@ -29,3 +29,21 @@ let average=sum/5;
 console.log("Sum : " + sum); 
 console.log("Average: " + average);  
 console.log("--------------------------------------------"); 
+
+//UC5- Unit Conversion 
+//a. 1ft = 12 in then 42 in = ? ft 
+let inches=42;
+let ft=inches/12;
+console.log("Conversion inches into ft : " + ft); 
+//b. Rectangular Plot of 60 feet x 40 feet in meters
+let feet1=60; 
+let feet2=40;
+let meters1=60 /3.281; 
+let meters2=40 /3.281;
+let product=meters1 * meters2; 
+console.log("Conversion feet into meter: " + product); 
+//c. Calculate area of 25 such plots in acres
+let area=25;
+let result1=area / 43560;
+console.log("Result : " + result1);
+console.log("--------------------------------------------"); 
