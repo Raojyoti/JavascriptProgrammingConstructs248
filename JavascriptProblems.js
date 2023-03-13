8bfe325486398948504763a00c62a75d200016da
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2023-03-13 17:10:02 +0530

    [Jyoti Rao] Add two random dice number

--- JavascriptProblems.js
+++ JavascriptProblems.js
@@ -1,8 +1,16 @@
 //Sequence Practice Problems
 //UC1- Get random Single digit 
-//let result=Math.floor(Math.random() * 10);
-//console.log(result);
+let result=Math.floor(Math.random() * 10);
+console.log(result);
 
 //UC2- Use Random to get Dice Number between 1 to 6
 let dice=Math.floor(Math.random() * 6)+1;
 console.log(dice); 
+
+//UC3- Add two Random Dice Number and Print the Result
+let dice1=Math.floor(Math.random() * 6)+1;
+console.log(dice1);
+let dice2=Math.floor(Math.random() * 6)+1;
+console.log(dice2);
+let sum=dice1 + dice2;
+console.log("Sum of two random dice: " + sum); 
