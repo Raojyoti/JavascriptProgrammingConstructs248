9ae6fb436826d2912b6843e3bfddf797ab4968b1
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2023-03-13 18:43:55 +0530

    [Jyoti Rao] Read number and print unit

--- JavascriptProblems.js
+++ JavascriptProblems.js
@@ -340,3 +340,44 @@ switch(dayNum)
             break;
 } 
 console.log("--------------------------------------------"); 
+
+//3.Read a Number 1, 10, 100, 1000, etc and display unit, ten, hundred,...
+
+let no = Math.floor(Math.random()*1050);
+if(no >=0 && no <=9)
+{
+   option=1;
+}
+else if(no >=10 && no <=99)
+{
+   option=2;
+}
+else if(no >=100 && no <=999)
+{
+   option=3;
+}
+else
+{
+   option=4;
+}
+switch(option)
+{
+    case 1:
+            console.log("Generate random number : "+ no);
+            console.log("Unit");
+            break;
+    case 2:
+            console.log("Generate random number : "+ no);
+            console.log("Ten");
+            break;
+    case 3:
+            console.log("Generate random number : "+ no);
+            console.log("Hundred");
+            break;
+    case 4:
+            console.log("Generate random number : "+ no);
+            console.log("Thousand");
+            break;
+} 
+
+console.log("--------------------------------------------"); 
