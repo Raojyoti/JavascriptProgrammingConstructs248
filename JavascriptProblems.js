05743b731e52d437ce20debc7025d5e42a6ad5c1
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2023-03-13 18:31:15 +0530

    [Jyoti Rao] Read single digit number and display in word

--- JavascriptProblems.js
+++ JavascriptProblems.js
@@ -256,3 +256,50 @@ console.log("--------------------------------------------");
  let output3= ((x * y) + z);
  console.log("Result4 are : " + output3); 
  console.log("--------------------------------------------"); 
+
+ //Selection Practice Problems with case statements
+//1. Read a single digit number and write the number in word using Case
+let number = Math.floor(Math.random()*10);
+switch(number)
+{
+    case 0:
+            console.log("Generate random number : "+ number);
+            console.log("Word ==> Zero");
+            break;
+    case 1:
+            console.log("Generate random number : "+ number);
+            console.log("Word ==> One");
+            break;
+    case 2:
+            console.log("Generate random number : "+ number);
+            console.log("Word ==> Two");
+            break;
+    case 3:
+            console.log("Generate random number : "+ number);
+            console.log("Word ==> Three");
+            break;
+    case 4:
+            console.log("Generate random number : "+ number);
+            console.log("Word ==> Four");
+            break;
+    case 5:
+           console.log("Generate random number : "+ number);
+            console.log("Word ==> Five");
+            break;
+    case 6:
+            console.log("Generate random number : "+ number);
+            console.log("Word ==> Six");
+            break;
+    case 7:
+            console.log("Generate random number : "+ number);
+            console.log("Word ==> Seven");
+            break;
+    case 8:
+            console.log("Generate random number : "+ number);
+            console.log("Word ==> Eight");
+            break;
+    case 9:
+            console.log("Generate random number : "+ number);
+            console.log("Word ==> Nine");
+            break;
+} 
