8a88c4f03aef95ba6949f3125d432491660d3e35
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2023-03-13 19:03:53 +0530

    [Jyoti Rao] Find Factorial number

--- JavascriptProblems.js
+++ JavascriptProblems.js
@@ -446,3 +446,13 @@ if (primeFactorNum > 2)
     console.log( primeFactorNum);
  }
  console.log("--------------------------------------------"); 
+
+ //5.Write a program that computes a factorial of a number taken as input. Such as 5 Factorial – 5! = 1 * 2 * 3 * 4 * 5
+ let factNumber=5;
+let fact=1;
+for(i=1;i<=factNumber;i++)
+{
+    fact=fact*i;
+}
+console.log("\nFactorial of "+ factNumber+" is ==>  " + fact) 
+console.log("--------------------------------------------"); 
