75c42449118c73c81cf0e6e58acdbf86a475f1cd
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2023-03-13 19:07:40 +0530

    [Jyoti Rao] Find power of 2 using while loop

--- JavascriptProblems.js
+++ JavascriptProblems.js
@@ -456,3 +456,15 @@ for(i=1;i<=factNumber;i++)
 }
 console.log("\nFactorial of "+ factNumber+" is ==>  " + fact) 
 console.log("--------------------------------------------"); 
+
+//Repetition Practice Problems with while loop
+//1.Write a program that takes a command-line argument n and prints a table of the powers of 2 that are less than or equal to 2^n till 256 is reached..
+let powervalue=Math.floor(Math.random()*10);
+let n=0;
+while (n<=powervalue)
+   {
+    let powerValue=Math.pow(2,n);
+    console.log("power of 2^"+ n +" is " +powerValue);
+    n++
+   } 
+console.log("--------------------------------------------");    
