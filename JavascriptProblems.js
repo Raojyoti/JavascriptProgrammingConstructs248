5306dd29c1d3d607ba59986a66d94ccdcc81f5ab
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2023-03-13 18:48:36 +0530

    [Jyoti Rao] Print power of two

--- JavascriptProblems.js
+++ JavascriptProblems.js
@@ -381,3 +381,15 @@ switch(option)
 } 
 
 console.log("--------------------------------------------"); 
+
+//Repetition Practice Problems with for loop
+//1. Prints a table of the powers of 2
+
+let value=Math.floor(Math.random()*10);
+let i;
+for (i=0;i<=value;i++)
+   {
+    let power=Math.pow(2,i);
+    console.log("power of 2^"+ i +" is " +power);
+   }
+console.log("--------------------------------------------"); 
