4693fbfcf4f8f6f9dc59baef39faca0426da80f8
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2023-03-13 18:02:27 +0530

    [Jyoti Rao] prints true if day of month is between March 20 and June 20, false otherwise.

--- JavascriptProblems.js
+++ JavascriptProblems.js
@@ -86,3 +86,16 @@ else
     console.log("Greater number ==> "+number5);
 }
 console.log("--------------------------------------------"); 
+
+//2.Write a program that takes day and month from the command line and prints true if day of month is between March 20 and June 20, false otherwise.
+let month=5;
+let date=20;
+if(((month <= 6 && date <= 20) && ((month >= 3 && date <= 20) && (date<31))))
+{
+   console.log("True");
+}
+else
+{
+   console.log("False");
+} 
+console.log("--------------------------------------------"); 
