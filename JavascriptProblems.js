4b7df216f902f2b7486f0185fbba5b9531b3c755
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2023-03-17 13:53:15 +0530

    [Jyoti Rao] First generate 3 digit random number and find min and max value

--- JavascriptProblems.js
+++ JavascriptProblems.js
@@ -34,3 +34,42 @@ let average=sum1/5;
 console.log("Sum : " + sum1); 
 console.log("Average: " + average);  
 console.log("--------------------------------------------"); 
+
+//--Selection Practice Problems with if & else  
+//1.Write a program that reads 5 Random 3 Digit values and then outputs the minimum and the maximum value
+let number1=Math.floor(Math.random() * 900)+100;
+console.log("first number: "+number1);
+let number2=Math.floor(Math.random() * 900)+100;
+console.log("Second number: "+number2);
+let number3=Math.floor(Math.random() * 900)+100;
+console.log("third number: "+number3);
+let number4=Math.floor(Math.random() * 900)+100;
+console.log("four number: "+number4);
+let number5=Math.floor(Math.random() * 900)+100;
+console.log("five number: "+number5);
+
+if(number1> number2 && number1>number3 && number1> number4 && number1> number5 )
+{
+    console.log("Greater number ==> "+number1);
+}
+else if(number2> number1 && number2> number3 && number2> number4 && number2> number5 )
+{
+    console.log("Greater number ==> "+number2);
+}
+else if(number2> number1 && number2> number3 && number2> number4 && number2> number5  )
+{
+    console.log("Greater number ==> "+number2);
+}
+else if(number3> number1 && number3> number2 && number3> number4 && number3> number5 )
+{
+    console.log("Greater number ==> "+number3);
+}
+else if(number4> number1 && number4> number2 && number4> number3 && number4> number5 )
+{
+    console.log("Greater number ==> "+number4);
+}
+else
+{
+    console.log("Greater number ==> "+number5);
+}
+console.log("--------------------------------------------"); 
