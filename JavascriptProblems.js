6a2f6dd54de090bca0a8e854ee03711a74b244ed
Jyoti Rao <117527397+Raojyoti@users.noreply.github.com>
2023-03-17 14:18:47 +0530

    "[Jyoti Rao] First generate 3 digit random number and find min and max value"

--- JavascriptProblems.js
+++ JavascriptProblems.js
@@ -50,15 +50,17 @@ console.log("--------------------------------------------");
 
 //--Selection Practice Problems with if & else  
 //1.Write a program that reads 5 Random 3 Digit values and then outputs the minimum and the maximum value
-let number1=Math.floor(Math.random() * 1000);
+//--Selection Practice Problems with if & else  
+//1.Write a program that reads 5 Random 3 Digit values and then outputs the minimum and the maximum value
+let number1=Math.floor(Math.random() * 900)+100;
 console.log("first number: "+number1);
-let number2=Math.floor(Math.random() * 1000);
+let number2=Math.floor(Math.random() * 900)+100;
 console.log("Second number: "+number2);
-let number3=Math.floor(Math.random() * 1000);
+let number3=Math.floor(Math.random() * 900)+100;
 console.log("third number: "+number3);
-let number4=Math.floor(Math.random() * 1000);
+let number4=Math.floor(Math.random() * 900)+100;
 console.log("four number: "+number4);
-let number5=Math.floor(Math.random() * 1000);
+let number5=Math.floor(Math.random() * 900)+100;
 console.log("five number: "+number5);
 
 if(number1> number2 && number1>number3 && number1> number4 && number1> number5 )
