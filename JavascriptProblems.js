ec6e4944b4821a91568e254c002a45fc4bfe726c
Jyoti Rao <117527397+Raojyoti@users.noreply.github.com>
2023-03-17 13:45:30 +0530

    [Jyoti Rao] Find sum and average of 5 two digit random number

--- JavascriptProblems.js
+++ JavascriptProblems.js
@@ -19,13 +19,18 @@ console.log("Sum of two random dice: " + add);
 console.log("--------------------------------------------"); 
 
 //UC4-– Write a program that reads 5 Random 2 Digit values ,then find their sum and the average
-let num1=Math.floor(Math.random() * 100);
-let num2=Math.floor(Math.random() * 100);
-let num3=Math.floor(Math.random() * 100);
-let num4=Math.floor(Math.random() * 100);
-let num5=Math.floor(Math.random() * 100);
-let sum=num1 + num2 + num3 + num4 + num5;
-let average=sum/5;
-console.log("Sum : " + sum); 
+let num1=Math.floor(Math.random() * 90)+10;
+let num2=Math.floor(Math.random() * 90)+10;
+let num3=Math.floor(Math.random() * 90)+10;
+let num4=Math.floor(Math.random() * 90)+10;
+let num5=Math.floor(Math.random() * 90)+10;
+let sum1=num1 + num2 + num3 + num4 + num5;
+console.log("First number value ==> "+num1);
+console.log("Second number value ==> "+num2);
+console.log("First number value ==> "+num3);
+console.log("Second number value ==> "+num4);
+console.log("First number value ==> "+num5);
+let average=sum1/5;
+console.log("Sum : " + sum1); 
 console.log("Average: " + average);  
 console.log("--------------------------------------------"); 
